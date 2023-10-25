a7c2726138e49c5e1f32552a4d2b6ff29437d4a2
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-25 17:14:53 +0200

    update Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,21 @@
 import Image from 'next/image'
+import { twMerge } from 'tailwind-merge'
 
-const Logo = () => {
+const Logo = ({ className = '' }) => {
   return (
-    <div className='flex h-8 w-8 items-center justify-center rounded-full bg-white'>
-      <Image src='/bear-logo.png' alt='Bear Logo' width={50} height={50} />
+    <div
+      className={twMerge(
+        'flex h-14 w-14 items-center justify-center rounded-full bg-white',
+        className
+      )}
+    >
+      <Image
+        src='/bear-logo.png'
+        alt='Bear Logo'
+        width={50}
+        height={50}
+        priority
+      />
     </div>
   )
 }
