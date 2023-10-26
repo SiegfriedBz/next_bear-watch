3a5bfd8bf3b9f3ae773469fe34563cc3e4368f3e
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-26 19:10:48 +0200

    add ButtonMobileBurger

---
+++ src/components/ButtonMobileBurger.jsx
@@ -0,0 +1,19 @@
+const ButtonMobileBurger = ({ modalIsOpen, setModalIsOpen }) => {
+  return (
+    <button
+      id='button-mobile-burger'
+      className={`${
+        modalIsOpen ? 'toggle-mobile-btn' : ''
+      } relative right-0 ms-4 h-8 w-8 rounded-2xl text-3xl`}
+      onClick={() => setModalIsOpen((prev) => !prev)}
+    >
+      <div
+        className={`absolute top-3.5 h-1 w-4 ${
+          !modalIsOpen ? 'animate-pulse' : ''
+        } bg-success rounded transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-slate-900 before:transition-all before:duration-500 before:content-[""] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-slate-900 after:transition-all after:duration-500 after:content-[""] before:dark:bg-stone-100 after:dark:bg-stone-100`}
+      ></div>
+    </button>
+  )
+}
+
+export default ButtonMobileBurger
