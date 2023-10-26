2a135109b414b9448d875b4539533106f4bab4f8
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-26 19:18:31 +0200

    add Features in Home page

---
+++ src/components/Features.jsx
@@ -0,0 +1,41 @@
+const Features = () => {
+  return (
+    <>
+      <h2 className='font-bold'>Explore Bear Watch&apos;s Key Features</h2>
+      <p>
+        Bear Watch is your ultimate companion for outdoor adventures. Whether
+        you&apos;re an enthusiastic hiker, seasoned camper, or a nature lover,
+        our app offers three essential features to keep you safe and connected:
+      </p>
+
+      <div>
+        <h3 className='font-bold'>Explore Bear Sightings</h3>
+        <p>
+          View bear markers added by fellow hikers. Gain insights into recent
+          bear sightings, so you can make informed decisions about your route
+          and stay vigilant during your outdoor journey.
+        </p>
+      </div>
+      <div>
+        <h3 className='font-bold'>Mark Your Bear Encounters</h3>
+        <p>
+          Easily add your own bear markers by simply tapping on the map. Share
+          your experience and help create a community-driven database of bear
+          encounters to benefit all outdoor enthusiasts.
+        </p>
+      </div>
+      <div>
+        <h3 className='font-bold'>Get Help When You Need It</h3>
+        <p>
+          The <span className='font-semibold italic'> Get Help feature</span> is
+          your lifeline in emergencies. Quickly send your current location to a
+          trusted friend, complete with your blood group information and a link
+          to your precise coordinates on Google Maps. Stay connected and ensure
+          a swift response when you require assistance.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Features
