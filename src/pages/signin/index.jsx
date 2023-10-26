94fec65499a05b3b33cf536ce9eb16dd0b751bef
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-26 19:16:16 +0200

    update Signin page

--- src/pages/signin/index.jsx
+++ src/pages/signin/index.jsx
@@ -1,21 +1,28 @@
+import PageLayout from '@/components/layouts/PageLayout'
 import { signIn } from 'next-auth/react'
 import Image from 'next/image'
 
 const Signin = () => {
   return (
-    <div
-      className='flex cursor-pointer items-center justify-center space-x-4'
-      onClick={() => signIn('google', { callbackUrl: '/' })}
-    >
-      Sign in with Google
-      <Image
-        src='/google_g_icon.png'
-        alt='Google Icon'
-        width={50}
-        height={50}
-        className='rounded-full'
-      />
-    </div>
+    <PageLayout>
+      <div className='layout-gradient flex flex-col items-center justify-center pb-80'>
+        <h2 className='text-3xl'>Sign in</h2>
+
+        <div
+          className='flex cursor-pointer items-center justify-center space-x-4'
+          onClick={() => signIn('google', { callbackUrl: '/' })}
+        >
+          <h3 className='font-semibold'>Sign in with Google</h3>
+          <Image
+            src='/google_g_icon.png'
+            alt='Google Icon'
+            width={35}
+            height={35}
+            className='rounded-full'
+          />
+        </div>
+      </div>
+    </PageLayout>
   )
 }
 
