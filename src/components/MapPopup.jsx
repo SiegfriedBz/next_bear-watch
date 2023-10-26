5aa1fc44a5434c5cf77e82b0a81bdd075fef245d
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-26 20:50:41 +0200

    update MapPopup

--- src/components/MapPopup.jsx
+++ src/components/MapPopup.jsx
@@ -1,18 +1,18 @@
 import { bearWasSeenWithinLastweek } from '@/utils/bearWasSeenWithinLastweek'
 import { formatDistanceToNow } from 'date-fns'
-import Logo from './Logo'
+import { Logo } from './Logo'
 
 const MapPopup = ({ popup }) => {
   return (
-    <div className='flex h-16 w-auto flex-col items-center justify-center'>
+    <div className='flex h-24 w-auto flex-col items-center justify-center rounded-md p-1'>
       <Logo
         className={
           bearWasSeenWithinLastweek(popup?.createdAt)
-            ? 'ring-warning bg-warning-light h-12 w-12 rounded-full ring-2'
-            : 'ring-success h-10 w-10 ring-2'
+            ? 'h-12 w-12 rounded-full bg-warning-light ring-2 ring-warning '
+            : 'h-10 w-10 ring-2 ring-success'
         }
       />
-      <p className='text-slate-900 dark:text-slate-900'>
+      <p className='m-0 text-slate-900 dark:text-slate-900'>
         {formatDistanceToNow(new Date(popup?.createdAt))} ago
       </p>
     </div>
