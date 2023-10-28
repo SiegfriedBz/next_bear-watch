dea99d1ae462b19789c8a0f959444a9293ef716e
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-28 14:05:01 +0200

    fix navlinks

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -67,7 +67,7 @@ const Li = ({ href = '', onClick, children }) => {
       <Link
         href={href}
         target='_self'
-        className='my-2 inline-block text-3xl font-extrabold'
+        className='my-2 inline-flex items-center justify-center text-3xl font-extrabold'
       >
         {children}
       </Link>
