9fa24ccc417b5b7c106259ae76dd2d4caee922ca
siegfriedbz <siegfried.bozza@yahoo.com>
2023-10-29 20:32:02 +0100

    change tw primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,15 @@ module.exports = {
         'cfg-white': '#f5f5f4', // stone-100
         'cfg-black': '#0f172a', // slate-900
         primary: {
-          DEFAULT: '#34d399', // emerald-400
-          dark: '#10b981', // emerald-500
-          light: '#6ee7b7', // emerald-300
+          // DEFAULT: '#34d399', // emerald-400
+          // dark: '#10b981', // emerald-500
+          // light: '#6ee7b7', // emerald-300
+          // DEFAULT: '#10b981', // emerald-500
+          // dark: '#059669', // emerald-600
+          // light: '#34d399', // emerald-400
+          DEFAULT: '#059669', // emerald-600
+          dark: '#047857', // emerald-700
+          light: '#10b981', // emerald-500
         },
         secondary: {
           DEFAULT: '#f59e0b', // amber-500
